bb9891000e9a5dc1837f8d533cd537c45a95e845
wang <wang@LAPTOP-2L3G88ML>
2023-01-30 19:27:09 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//                 计算 n的阶乘
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++) {
+//		ret = ret* i;
+//	}
+//	printf("ret=%d\n", ret);
+//	return 0;
+//}
+
+
+
+//          计算 1!+ 2!+ 3!+ …… + 10!
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//		
+//
+//	}
+//
+//	printf("sum=%d", sum);
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	int n = 0;
+	int ret = 1;
+	int sum = 0;
+	for (n = 1; n <= 10; n++)
+	{
+		ret = ret * n;
+		
+		sum = sum + ret;
+
+
+	}
+
+	printf("sum=%d", sum);
+	return 0;
+}
+
+	
